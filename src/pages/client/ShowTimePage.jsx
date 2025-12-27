94c3d87712ec5b799606eaf0de6239af4fa8db74
phongnbn <phongnbn123@gmail.com>
2025-12-28 00:39:14 +0700

    Update lich chieu + hover do tuoi

--- src/pages/client/ShowTimePage.jsx
+++ src/pages/client/ShowTimePage.jsx
@@ -7,7 +7,7 @@ import {
   getDetailMovie,
   getAllMovie,
 } from "../../common/services/movie.service";
-import { Modal } from "antd";
+import { Modal, Tooltip } from "antd";
 import { PlayCircleOutlined } from "@ant-design/icons";
 import { getAgeBadge } from "../../common/utils/age";
 
@@ -102,9 +102,19 @@ const ShowtimePage = () => {
                   : "Sắp chiếu"}
               </span>
               {(movie.ageRequire || movie.ageRestriction) && (
-                <span className="bg-yellow-500 text-black px-3 py-1 rounded text-xs font-bold">
-                  {getAgeBadge(movie.ageRequire || movie.ageRestriction).label}
-                </span>
+                <Tooltip
+                  title={
+                    getAgeBadge(movie.ageRequire || movie.ageRestriction)
+                      .description
+                  }
+                >
+                  <span className="bg-yellow-500 text-black px-3 py-1 rounded text-xs font-bold cursor-help">
+                    {
+                      getAgeBadge(movie.ageRequire || movie.ageRestriction)
+                        .label
+                    }
+                  </span>
+                </Tooltip>
               )}
             </div>
 
